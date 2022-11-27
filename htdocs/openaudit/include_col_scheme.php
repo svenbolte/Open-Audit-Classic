4a85ff5c1fb8163e6c92d825b71371a718b2ce98
Sven Bolte <67223729+svenbolte@users.noreply.github.com>
2022-11-27 18:43:22 +0100

    css layout improved

--- htdocs/openaudit/include_col_scheme.php
+++ htdocs/openaudit/include_col_scheme.php
@@ -1,9 +1,9 @@
 <?php
-  $header_top = "background-color: rgba(192,192,192,0.2);";
+  $header_top = "background-color: #00606022";
   $header_top_image = "images/header_top_blue.jpg";
-  $header_middle = "background-color: rgba(192,192,192,0.2);";
+  $header_middle = "background-color: #00606022";
   $header_middle_image = "images/header_middle_blue.jpg";
-  $header_bottom = "background-color: rgba(192,192,192,0.2);";
+  $header_bottom = "background-color: #00606022";
   $background = "#EEEEEE";
   $background_image = "";
   $h1 = "777777";
@@ -13,7 +13,7 @@
   $but_bac = "images/go-prev.png";
   $but_all = "images/go-all.png";
   $bgcolor = "#FFFFFF";
-  $bg1 = "rgba(192,192,192,0.2)";
+  $bg1 = "#00606022";
   $bg2 = "#FFFFFF";
 
 //testing
